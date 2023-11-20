24f684117281b91ef13ece25c017bc69a1961ca1
Mirco Giardina <mirco.giardina@ennova-research.com>
2023-11-20 12:00:39 +0100

    Update request dto

--- src/main/java/net/ielpo/roadmap/dto/HelloWorldRequestDto.java
+++ src/main/java/net/ielpo/roadmap/dto/HelloWorldRequestDto.java
@@ -1,8 +1,14 @@
 package net.ielpo.roadmap.dto;
 
 public class HelloWorldRequestDto extends BaseResponseOkDto {
+
+    // Da mettere sempre, non si sa mai
+    public HelloWorldRequestDto() {
+    }
+
     // @JsonProperty("body") => JsonProperty può essere usato per cambiare il nome
     // del campo nel payload ricevuto. Crea un automatico il getter e setter
+    // Eventualmente dichiarare direttamente public
     private String[] payload;
 
     public String[] getPayload() {
