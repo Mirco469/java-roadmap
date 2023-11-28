1a3ea39a1b6b3876654dccaa23e76cf92d678729
Mirco Giardina <mirco.giardina@ennova-research.com>
2023-11-28 15:33:25 +0100

    Exercise 3

---
+++ src/test/java/net/ielpo/roadmap/Excercise3.java
@@ -0,0 +1,27 @@
+package net.ielpo.roadmap;
+
+public class Excercise3 {
+    public static void asciiArt() {
+        Integer totalRows = 10;
+
+        for (Integer row = 0; row < totalRows; row++) {
+            // Print @
+            for (Integer at = 0; at < row; at++) {
+                System.out.print('@');
+            }
+            // Print brackets
+            if (row % 3 == 0 || row % 6 == 0) {
+                System.out.print("[]");
+            } else {
+                System.out.print("][");
+            }
+            // Print #
+            for (Integer hashtag = row; hashtag < totalRows - 1; hashtag++) {
+                System.out.print('#');
+            }
+
+            // New Line
+            System.out.println();
+        }
+    }
+}
